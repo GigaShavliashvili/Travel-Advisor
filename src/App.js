dec734aeecb2a43439dd91215da87e6f50dc6696
GigaShavliashvili <87610428+GigaShavliashvili@users.noreply.github.com>
2022-06-02 13:40:50 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  navigate,
+} from "react-router-dom";
+import { fetchCity } from "./api";
+import MapPage from "./Page/MapPage";
+import Home from "./Page/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Map/:id" element={<MapPage />}></Route>
+        {/*   <Route
+          path="/"
+          element={
+            isAuthentication ? (
+              <Navigate replace to="/Dashboard" />
+            ) : (
+              <Navigate replace to="/Signin" />
+            )
+          }
+        ></Route> */}
+
+        {/*         <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
